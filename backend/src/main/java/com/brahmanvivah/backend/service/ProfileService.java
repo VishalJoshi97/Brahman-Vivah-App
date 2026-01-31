5ebd49b8f9c00451b45692d09d471e7a8d875561
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-01-31 23:57:50 +0530

    createProfile for on boarding profile service.

--- backend/src/main/java/com/brahmanvivah/backend/service/ProfileService.java
+++ backend/src/main/java/com/brahmanvivah/backend/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.brahmanvivah.backend.service;
 //from dto
 import com.brahmanvivah.backend.dto.ProfileOnBoardingRequest;
 
+import com.brahmanvivah.backend.enums.*;
 import com.brahmanvivah.backend.model.Profile;
 import com.brahmanvivah.backend.model.User;
 import com.brahmanvivah.backend.repository.ProfileRepository;
@@ -27,18 +28,18 @@ public class ProfileService {
 
     //add new profile to repository=>model
     //any changes in service=>save to repo=>updates db
+    //must use dto mapping
     @Transactional
     public Profile createProfile(Long userId, ProfileOnBoardingRequest request) {//dto=>repository=>model
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        if (profileRepository.existsByUser(user.getId())) {
+        if (profileRepository.existsByUser(userId) ){
             throw new RuntimeException("Profile already exists for this user");
         }
 
-        //Create profile and attach user
-        //repository=>model
+        //entity.set(dto.get)
         Profile profile = new Profile();
 
         profile.setUser(user); // ⭐ THIS sets user_id
@@ -46,31 +47,46 @@ public class ProfileService {
         profile.setCaste(request.getCaste());
         profile.setSubCaste(request.getSubCaste());
         profile.setGotra(request.getGotra());
+        profile.setChoice(request.getChoice());
+        profile.setSexualOrientation(request.getSexualOrientation());
+        profile.setHobbies(request.getHobbies());
+        profile.setHabits(request.getHabits());
+
         return profileRepository.save(profile);
     }
 
+
+//separate endpoints for each field
     //get profile by id
     public Profile getProfileById(Long id){
         return profileRepository.findById(id)
                 .orElseThrow(()->new RuntimeException("Profile not found"));
     }
     //get profile by caste
-    public List<Profile> getProfileByCaste(String caste) {
+    public List<Profile> getProfileByCaste(Caste caste) {
         return profileRepository.findByCaste(caste);
     }
 
     //get profile by subcaste
-    public List<Profile> getProfileBySubCaste(String subCaste){
+    public List<Profile> getProfileBySubCaste(SubCaste subCaste){
         return profileRepository.findBySubCaste(subCaste);
     }
     //get profile by gotra
-    public List<Profile> getProfileByGotra(String gotra){
-        System.out.println("Gotra: "+gotra);
-        return profileRepository.findByGotra(gotra);
+    public List<Profile> getProfileBySexualOrientation(sexualOrientation){
+        System.out.println("Gotra: "+sexualOrientation);
+        return profileRepository.findBySexualOrientation(sexualOrientation);
     }
 
-    public List<Profile> getProfileByCity(String city) {
-        return profileRepository.findByCity(city);
+    //get profile by choices
+    public List<Profile> getProfileByHobbies(Hobbies hobbies){
+        System.out.println("Hobbies: "+hobbies);
+        return profileRepository.findByAllHobbies(hobbies);
+    }
+
+    //get profile by choices
+    public List<Profile> getProfileByHabits(Gotra gotra){
+        System.out.println("Gotra: "+gotra);
+        return profileRepository.findByGotra(gotra);
     }
 
 }
